b32a233c9e4a5bbe4bb7682257054e344df7e7ed
Loïc Pfister <pfister.loic@gmail.com>
2020-08-06 10:00:49 +0200

    Update server.js

--- server.js
+++ server.js
@@ -73,4 +73,4 @@ app.use('/graphql', graphqlHTTP({
   rootValue: root,
   graphiql: true,
 }));
-app.listen(4000, () => console.log('Now browse to localhost:4000/graphql'));
+app.listen(443, () => console.log('Now browse to localhost:4000/graphql'));
